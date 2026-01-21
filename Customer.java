80478c164ddf83c192811a63eba536c25f8d8901
tranleanhkhoa041221-cpu <tranleanhkhoa041221@gmail.com>
2026-01-21 13:24:50 +0700

    Upload btvnsession7_xuatsac1

---
+++ Customer.java
@@ -0,0 +1,12 @@
+package BaiTapXuatSac1;
+
+public class Customer {
+        private int id;
+        private String name;
+        private String email;
+
+        public void showInfo() {
+
+    }
+
+}
---
+++ Order.java
@@ -0,0 +1,13 @@
+package BaiTapXuatSac1;
+
+public class Order {
+    private String orderId;
+    private Customer customer;
+    private double total;
+
+        public void printOrder() {
+
+
+    }
+
+}
