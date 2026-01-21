034c31a84d1e90dd5792e3bece5f3867b3f7384b
tranleanhkhoa041221-cpu <tranleanhkhoa041221@gmail.com>
2026-01-21 13:35:49 +0700

    upload btvnsession7_xuatsac2

---
+++ Course.java
@@ -0,0 +1,17 @@
+package BaiTapXuatSac2;
+
+public class Course {
+        private String code;
+        private String title;
+
+        public Course(String code, String title) {
+            this.code = code;
+            this.title = title;
+        }
+
+        public void showCourse() {
+
+        }
+    }
+
+
---
+++ Student.java
@@ -0,0 +1,18 @@
+package BaiTapXuatSac2;
+
+public class Student {
+        private String id;
+        private String name;
+        private Course course;
+
+        public Student(String id, String name, Course course) {
+            this.id = id;
+            this.name = name;
+            this.course = course;
+        }
+
+        public void displayInfo() {
+
+        }
+    }
+
